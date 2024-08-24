65a27137d89db08e608f4234764f7293fdc6f3ed
tarunsbharwal <sabharwaltarun786@gmail.com>
2024-08-24 22:54:16 +0530

    initial commit

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB7PUr8yDEc5kt5eaT359mfulqhnbYTTcI",
+    authDomain: "crypto-tracker-fd440.firebaseapp.com",
+    projectId: "crypto-tracker-fd440",
+    storageBucket: "crypto-tracker-fd440.appspot.com",
+    messagingSenderId: "540355574092",
+    appId: "1:540355574092:web:a4062ce4bb3e6d4e4877e9"
+  };
+//import { initializeApp } from "firebase/app";
+// Initialize Firebase
+//const app = initializeApp(firebaseConfig);
+
+  export default firebaseConfig;
